2aa973879ed244fa21100bf2acabf528250cfbaf
yearbukservices <yearbukservices@gmail.com>
2026-08-27 20:43:41 +0100

    Persist recent searches per user

--- client/src/pages/dashboard/search-page.tsx
+++ client/src/pages/dashboard/search-page.tsx
@@ -128,6 +128,7 @@ export default function SearchPage({ onRegisterReset, forceSchoolProfile, forceV
             
             <AdvancedSearch
               schools={schools}
+              isAuthenticated={!!user}
               onSchoolClick={handleSchoolSelect}
               onUserClick={handleUserSelect}
             />
