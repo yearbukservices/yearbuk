fa30ebc0d4522c6e5602ae372aae9ecd5d4fd01b
yearbukservices <yearbukservices@gmail.com>
2026-08-31 09:11:28 +0100

    Update YearbookConfigDialog.tsx

--- client/src/components/YearbookConfigDialog.tsx
+++ client/src/components/YearbookConfigDialog.tsx
@@ -340,7 +340,7 @@ export function YearbookConfigDialog({
                       </div>
                     </RadioGroup>
                     <p className="text-xs text-blue-100 mt-3">
-                      This choice isn’t permanent—you can change the upload type later if needed.
+                      This choice isn’t permanent— you can change the upload type later.
                     </p>
                   </CardContent>
                 </Card>
