546a4300d3bf146e30882d05f8d92f2bb1228d70
yearbukservices <yearbukservices@gmail.com>
2026-08-31 10:22:47 +0100

    Use Yearbuk logo in dashboard header

--- client/src/components/DashboardLayout.tsx
+++ client/src/components/DashboardLayout.tsx
@@ -4,7 +4,7 @@ import { useQuery, useMutation } from "@tanstack/react-query";
 import { queryClient, apiRequest } from "@/lib/queryClient";
 import { Button } from "@/components/ui/button";
 import { Bell, Menu, ShoppingCart, Settings, Home, LogOut, X, Library, User, Upload, Users, Search, BookOpen, Image as ImageIcon, Package, UserCheck } from "lucide-react";
-import logoImage from "@assets/logo_background_null.png";
+import logoImage from "@assets/chrome_search_logo.png";
 import type { Notification, User as UserType, AlumniBadge } from "@shared/schema";
 import { useToast } from "@/hooks/use-toast";
 import { Copy, Calendar } from "lucide-react";
